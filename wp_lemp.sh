b66ab23182c52a5f66fc83545d129d881af7398f
shubhamch71 <83663663+shubhamch71@users.noreply.github.com>
2023-07-28 12:28:15 +0530

    Update wp_lemp.sh

--- wp_lemp.sh
+++ wp_lemp.sh
@@ -127,7 +127,7 @@ EOF
     done
 
     # Prompt the user to open the site in a browser
-    echo "The site is up and healthy. Do you want to open http://$site_name in your browser? (y/n). To exit lynx you can traverse to the bottom of page using down arrow key and then press q to quit and press y"
+    echo "The site is up and healthy. Do you want to open http://$site_name in your browser (lynx)? (y/n). After viewing the site , to exit lynx you can traverse to the bottom of page using down arrow key and then press q to quit and press y"
     read -r choice
     if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
 	sleep 2
